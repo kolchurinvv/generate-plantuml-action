282c2c255715b1077a8d62245d918624890e4583
Vladimir Kolchurin <18503099+kolchurinvv@users.noreply.github.com>
2024-02-28 14:04:16 +0100

    udpated to work with esmodules; custom surever and basic authentication

--- src/main.ts
+++ src/main.ts
@@ -1,102 +1,153 @@
 // TODO logging.
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-const axios = require('axios');
-import { Base64 } from 'js-base64';
-const path = require('path');
-const plantumlEncoder = require('plantuml-encoder');
+import * as core from "@actions/core"
+import * as github from "@actions/github"
+import axios from "axios"
+import { Base64 } from "js-base64"
+import path from "path"
+import plantumlEncoder from "plantuml-encoder"
 
-import { retrieveCodes, getCommitsFromPayload, updatedFiles } from './utils';
-// import { GitHub } from '@actions/github/lib/utils';
+import {
+  retrieveCodes,
+  getCommitsFromPayload,
+  updatedFiles,
+  NetworkError,
+} from "./utils.js"
 
-async function generateSvg(code) {
-    const encoded = plantumlEncoder.encode(code);
-    try {
-        const res = await axios.get(`http://www.plantuml.com/plantuml/svg/${encoded}`);
-        return res.data;
-    } catch(e) {
-        // TODO
-    }
+const diagramPath = core.getInput("path")
+const commitMessage = core.getInput("message")
+const server = core.getInput("server")
+const username = core.getInput("username")
+const password = core.getInput("password")
+
+type GenerateSvgPayload = {
+  code: string
+  server?: string
+  username?: string
+  password?: string
 }
 
-const diagramPath = core.getInput('path');
-const commitMessage = core.getInput('message');
-
-console.log("input path:", diagramPath);
-if (!process.env.GITHUB_TOKEN) {
-    core.setFailed('Please set GITHUB_TOKEN env var.');
-    process.exit(1);
-}
-// const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
-// const octokit = new GitHub(getOctokitOptions(process.env.GITHUB_TOKEN))
-const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-
-(async function main() {
-    const payload = github.context.payload;
-    const ref     = payload.ref;
-    if (!payload.repository) {
-        throw new Error();
-    }
-    const owner   = payload.repository.owner.login;
-    const repo    = payload.repository.name;
-
-    const commits = await getCommitsFromPayload(octokit, payload);
-    const files = updatedFiles(commits);
-    const plantumlCodes = retrieveCodes(files);
-
-    let tree: any[] = [];
-    for (const plantumlCode of plantumlCodes) {
-        const p = path.format({
-            dir: (diagramPath === '.') ? plantumlCode.dir : diagramPath,
-            name: plantumlCode.name,
-            ext: '.svg'
-        });
-
-        const svg = await generateSvg(plantumlCode.code);
-        const blobRes = await octokit.rest.git.createBlob({
-            owner, repo,
-            content: Base64.encode(svg),
-            encoding: 'base64',
-        });
-
-        const sha = await octokit.rest.repos.getContent({
-            owner, repo, ref, path: p
-        }).then(res => (<any>res.data).sha).catch(e => undefined);
-
-        if (blobRes.data.sha !== sha) {
-            tree = tree.concat({
-                path: p.toString(),
-                mode: "100644",
-                type: "blob",
-                sha: blobRes.data.sha
-            })
+async function generateSvg(payload: GenerateSvgPayload) {
+  const encoded = plantumlEncoder.encode(payload.code)
+  try {
+    let res
+    let headers = {}
+    if (payload.server) {
+      if (payload.username && payload.password) {
+        headers = {
+          auth: {
+            username: core.getInput("username"),
+            password: core.getInput("password"),
+          },
         }
+      }
+      res = await axios.get(`${server}/svg${encoded}`, headers)
+    } else {
+      res = await axios.get(`http://www.plantuml.com/plantuml/svg/${encoded}`)
+      return res.data
     }
-
-    if (tree.length === 0) {
-        console.log(`There are no files to be generated.`);
-        return;
+  } catch (e) {
+    if (e instanceof Error || e instanceof NetworkError) {
+      core.setFailed(e)
     }
+    if (typeof e === "object" && e !== null && "message" in e) {
+      throw new NetworkError(e.message)
+    }
+    throw new NetworkError("Unknown error")
+  }
+}
 
-    const treeRes = await octokit.rest.git.createTree({
-        owner, repo, tree,
-        base_tree: commits[commits.length - 1].commit.tree.sha,
-    });
+if (!process.env.GITHUB_TOKEN) {
+  core.setFailed("Please set GITHUB_TOKEN env var.")
+  process.exit(1)
+}
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
 
-    const createdCommitRes = await octokit.rest.git.createCommit({
-        owner, repo,
-        message: commitMessage,
-        parents: [ commits[commits.length - 1].sha ],
-        tree: treeRes.data.sha,
-    });
+;(async function main() {
+  const payload = github.context.payload
+  const ref = payload.ref
+  if (!payload.repository) {
+    throw new Error('Unable to get "repository" from payload.')
+  }
+  const owner = payload.repository.owner.login
+  const repo = payload.repository.name
 
-    const updatedRefRes = await octokit.rest.git.updateRef({
-        owner, repo,
-        ref: ref.replace(/^refs\//, ''),
-        sha: createdCommitRes.data.sha,
-    });
+  const commits = await getCommitsFromPayload(octokit, payload)
+  const files = updatedFiles(commits)
+  const plantumlCodes = retrieveCodes(files)
 
-    console.log(`${tree.map(t => t.path).join("\n")}\nAbove files are generated.`);
-})().catch(e => {
-    core.setFailed(e);
-});
+  let tree: any[] = []
+  for (const plantumlCode of plantumlCodes) {
+    const p = path.format({
+      dir: diagramPath === "." ? plantumlCode.dir : diagramPath,
+      name: plantumlCode.name,
+      ext: ".svg",
+    })
+    const svgPayload: GenerateSvgPayload = { code: plantumlCode.code }
+    if (server) {
+      svgPayload.server = server
+    }
+    if (username && password) {
+      svgPayload.username = username
+      svgPayload.password = password
+    }
+    const svg = await generateSvg(plantumlCode.code)
+    const blobRes = await octokit.rest.git.createBlob({
+      owner,
+      repo,
+      content: Base64.encode(svg),
+      encoding: "base64",
+    })
+
+    const sha = await octokit.rest.repos
+      .getContent({
+        owner,
+        repo,
+        ref,
+        path: p,
+      })
+      .then((res) => (<any>res.data).sha)
+      .catch((e) => undefined)
+
+    if (blobRes.data.sha !== sha) {
+      tree = tree.concat({
+        path: p.toString(),
+        mode: "100644",
+        type: "blob",
+        sha: blobRes.data.sha,
+      })
+    }
+  }
+
+  if (tree.length === 0) {
+    console.log(`There are no files to be generated.`)
+    return
+  }
+
+  const treeRes = await octokit.rest.git.createTree({
+    owner,
+    repo,
+    tree,
+    base_tree: commits[commits.length - 1].commit.tree.sha,
+  })
+
+  const createdCommitRes = await octokit.rest.git.createCommit({
+    owner,
+    repo,
+    message: commitMessage,
+    parents: [commits[commits.length - 1].sha],
+    tree: treeRes.data.sha,
+  })
+
+  const updatedRefRes = await octokit.rest.git.updateRef({
+    owner,
+    repo,
+    ref: ref.replace(/^refs\//, ""),
+    sha: createdCommitRes.data.sha,
+  })
+
+  console.log(
+    `${tree.map((t) => t.path).join("\n")}\nAbove files are generated.`
+  )
+})().catch((e) => {
+  core.setFailed(e)
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,100 +1,108 @@
-import fs from 'fs';
-import { uniq } from 'lodash';
-import path from 'path';
-const markdownit = require('markdown-it');
-const umlFileExtensions = [
-    '.pu',
-    '.pml',
-    '.puml',
-    '.plantuml',
-];
+import fs from "fs"
+import { uniq } from "lodash-es"
+import path from "path"
+// const markdownit = require("markdown-it")
+import markdownit from "markdown-it"
+const umlFileExtensions = [".pu", ".pml", ".puml", ".plantuml"]
 const markdownExtensions = [
-    '.md',
-    '.markdown',
-    '.mdown',
-    '.mkdn',
-    '.mdwn',
-    '.mkd',
-    '.mdn',
-    '.md.txt',
-];
-
+  ".md",
+  ".markdown",
+  ".mdown",
+  ".mkdn",
+  ".mdwn",
+  ".mkd",
+  ".mdn",
+  ".md.txt",
+]
+export class NetworkError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = "NetworkError"
+  }
+}
 export function retrieveCodes(files) {
-    return files.reduce((accum, f) => {
-        const p = path.parse(f);
-        if (umlFileExtensions.indexOf(p.ext) !== -1) {
-            return accum.concat({
-                name: p.name,
-                // TODO: files may have been deleted.
-                code: fs.readFileSync(f).toString(),
-                dir: p.dir
-            });
-        }
-        if (markdownExtensions.indexOf(p.ext) !== -1) {
-            // TODO: files may have been deleted.
-            const content = fs.readFileSync(f).toString();
-            const dir = path.dirname(f);
-            const codes = puFromMd(content);
-            codes.forEach(code => {
-                code.dir = path.dirname(f)
-                return code;
-            })
-            return accum.concat(codes);
-        }
-        return accum;
-    }, []);
+  return files.reduce((accum, f) => {
+    const p = path.parse(f)
+    if (umlFileExtensions.indexOf(p.ext) !== -1) {
+      return accum.concat({
+        name: p.name,
+        // TODO: files may have been deleted.
+        code: fs.readFileSync(f).toString(),
+        dir: p.dir,
+      })
+    }
+    if (markdownExtensions.indexOf(p.ext) !== -1) {
+      // TODO: files may have been deleted.
+      const content = fs.readFileSync(f).toString()
+      const dir = path.dirname(f)
+      const codes = puFromMd(content)
+      const formattedCodes = codes.map((code) => {
+        code.dir = dir
+        return code
+      })
+      return accum.concat(formattedCodes)
+    }
+    return accum
+  }, [])
 }
 
-const infoRegexp = /^plantuml(?:@(.+))?:([\w-_.]+)/;
+const infoRegexp = /^plantuml(?:@(.+))?:([\w-_.]+)/
 
 function puFromMd(markdown) {
-    const md = new markdownit();
-    const fences = md.parse(markdown, {})
-        .filter(token => token.type === 'fence')
-        .filter(token => infoRegexp.test(token.info));
+  const md = new markdownit()
+  const fences = md
+    .parse(markdown, {})
+    .filter((token) => token.type === "fence")
+    .filter((token) => infoRegexp.test(token.info))
 
-    return fences.reduce((accum, fence) => {
-        const [, umlType, name] = fence.info.match(infoRegexp) || [];
-        const [, typeInContent] = fence.content.match(/^(@start\w+)/) || [];
+  return fences.reduce((accum, fence) => {
+    const [, umlType, name] = fence.info.match(infoRegexp) || []
+    const [, typeInContent] = fence.content.match(/^(@start\w+)/) || []
 
-        if (!name) {
-            return accum;
-        }
-        if (typeInContent) {
-            return accum.concat({
-                name,
-                code: fence.content
-            })
-        }
-        const t = umlType || 'uml';
-        return accum.concat({
-            name,
-            code: [
-                `@start${t}`,
-                fence.content.trim(),
-                `@end${t}`,
-                ''
-            ].join("\n"),
-        })
-    }, []);
+    if (!name) {
+      return accum
+    }
+    if (typeInContent) {
+      return accum.concat({
+        name,
+        code: fence.content,
+      })
+    }
+    const t = umlType || "uml"
+    return accum.concat({
+      name,
+      code: [`@start${t}`, fence.content.trim(), `@end${t}`, ""].join("\n"),
+    })
+  }, [])
 }
 
 export async function getCommitsFromPayload(octokit, payload) {
-    const commits = payload.commits;
-    const owner   = payload.repository.owner.login;
-    const repo    = payload.repository.name;
+  const commits = payload.commits
+  const owner = payload.repository.owner.login
+  const repo = payload.repository.name
 
-    const res = await Promise.all(commits.map(commit => octokit.repos.getCommit({
-        owner, repo, ref: commit.id
-    })));
-    return res.map(res => (<any>res).data);
+  const res = await Promise.all(
+    commits.map((commit) =>
+      octokit.repos.getCommit({
+        owner,
+        repo,
+        ref: commit.id,
+      })
+    )
+  )
+  return res.map((res) => (<any>res).data)
 }
 
 export function updatedFiles(commits) {
-    return uniq(commits.reduce(
-        (accum: any[], commit) => accum.concat(
-            commit.files.filter(f => f.status !== 'removed').map(f => f.filename)
+  return uniq(
+    commits.reduce(
+      (accum: any[], commit) =>
+        accum.concat(
+          commit.files
+            .filter((f) => f.status !== "removed")
+            .map((f) => f.filename)
         ),
-        []
-    ));
+      []
+    )
+  )
 }
